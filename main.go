2b64093489faab511f3e7ea35daa4d8637de37a3
disco07 <koneenok@outlook.fr>
2022-08-22 23:56:08 +0200

    first commit

--- main.go
+++ main.go
@@ -13,10 +13,16 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 	}
 }
 
+func worker2(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
 func main() {
 	job := make(chan int, 10)
 	result := make(chan int, 10)
-	for w := 1; w <= 10; w++ {
+	for w := 1; w <= 9; w++ {
 		go worker(w, job, result)
 	}
 	for j := 1; j <= 9; j++ {
@@ -26,4 +32,16 @@ func main() {
 	for a := 1; a <= 9; a++ {
 		<-result
 	}
+
+	//var wg sync.WaitGroup
+	//for i := 1; i <= 9; i++ {
+	//	wg.Add(1)
+	//
+	//	i := i
+	//	go func() {
+	//		defer wg.Done()
+	//		worker2(i)
+	//	}()
+	//}
+	//wg.Wait()
 }
