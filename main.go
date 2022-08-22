e72b4098441258be5c8b805a9a013bef012f5ea6
disco07 <koneenok@outlook.fr>
2022-08-22 23:44:29 +0200

    first commit

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
 		fmt.Println("worker", id, "processing job", j)
 		time.Sleep(time.Second)
-		results <- j * 2
+		results <- j
 	}
 }
 
 func main() {
 	job := make(chan int, 10)
 	result := make(chan int, 10)
-	for w := 1; w <= 2; w++ {
+	for w := 1; w <= 10; w++ {
 		go worker(w, job, result)
 	}
 	for j := 1; j <= 9; j++ {
