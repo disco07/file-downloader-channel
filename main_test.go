5856d0798e5514ddbe43c208be34a9aa9e679b75
disco07 <koneenok@outlook.fr>
2022-08-26 10:38:34 +0200

    testing code

--- main_test.go
+++ main_test.go
@@ -6,22 +6,34 @@ import (
 	"testing"
 )
 
+var downloaderTestsNotError = []struct {
+	url      string
+	expected error
+}{
+	{"https://agritrop.cirad.fr/584726/1/Rapport.pdf", nil},
+}
+
 var downloaderTests = []struct {
 	url      string
 	expected error
 }{
-	//{"https://agritrop.cirad.fr/584726/1/Rapport.pdf", nil},
 	{"", fmt.Errorf("invalid url")},
 	{"https://youtu.be/w0NQlEMjntI", errors.New("unable to download file with multithreads")},
 	{"https://github.com/disco07/file-downloader", errors.New("unable to parse variable")},
 }
 
+func TestDownloaderNotError(t *testing.T) {
+	for _, tt := range downloaderTestsNotError {
+		err := downloader(tt.url)
+		if err != nil {
+			t.Fatalf("Expected error for input %v", tt.url)
+		}
+	}
+}
+
 func TestDownloader(t *testing.T) {
 	for _, tt := range downloaderTests {
 		err := downloader(tt.url)
-		if err == nil {
-			t.Errorf("Expected error for input %v", tt.url)
-		}
 		if err.Error() != tt.expected.Error() {
 			t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
 		}
