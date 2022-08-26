111607272f0baa98c804fbd4c5e575abf3754362
disco07 <koneenok@outlook.fr>
2022-08-26 08:11:34 +0200

    file downloader with channel

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 	"strings"
 )
 
+type filePart struct {
+	name       string
+	start, end int
+}
+
 func downloader(url string) error {
 	if strings.TrimSpace(url) == "" {
 		return errors.New("invalid url")
@@ -26,6 +31,7 @@ func downloader(url string) error {
 	}
 	urlSplit := strings.Split(url, "/")
 	filename := urlSplit[len(urlSplit)-1]
+	fmt.Println(filename)
 	if res.Header.Get("Accept-Ranges") != "bytes" {
 		return errors.New("impossible de télécharger ce fichier")
 	}
@@ -37,21 +43,23 @@ func downloader(url string) error {
 	nbPart := 5
 	offset := cntLen / nbPart
 
-	ch := make(chan os.File, nbPart)
+	jobs := make(chan filePart, nbPart)
+	results := make(chan string, nbPart)
+
+	for w := 0; w < nbPart; w++ {
+		go worker(url, jobs, results)
+	}
 
 	for i := 0; i < nbPart; i++ {
 		name := fmt.Sprintf("part%d", i)
 		start := i * offset
 		end := (i + 1) * offset
-		part, err := os.Create(name)
-		if err != nil {
-			return err
-		}
+		jobs <- filePart{name: name, start: start, end: end}
+	}
+	close(jobs)
 
-		go func() {
-			ch <- writeFile(url, start, end, *part)
-		}()
-		<-ch
+	for i := 0; i < nbPart; i++ {
+		fmt.Println(<-results)
 	}
 
 	out, err := os.Create(filename)
@@ -72,49 +80,55 @@ func downloader(url string) error {
 			return err
 		}
 	}
-	close(ch)
 
 	return nil
 }
 
-func writeFile(url string, start, end int, file os.File) os.File {
-	defer file.Close()
-	client := http.Client{}
+func worker(url string, jobs <-chan filePart, results chan<- string) {
+	for job := range jobs {
+		part, err := os.Create(job.name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		client := http.Client{}
 
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", job.start, job.end))
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := os.OpenFile(job.name, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bar := progressbar.DefaultBytes(
+			res.ContentLength,
+			"downloading",
+		)
+		io.Copy(io.MultiWriter(f, bar), res.Body)
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = part.Write(body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results <- part.Name()
+
+		f.Close()
+		res.Body.Close()
+		part.Close()
 	}
-
-	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", start, end))
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	f, err := os.OpenFile(file.Name(), os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	bar := progressbar.DefaultBytes(
-		res.ContentLength,
-		"downloading",
-	)
-	io.Copy(io.MultiWriter(f, bar), res.Body)
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = file.Write(body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return file
 }
 
 func main() {
