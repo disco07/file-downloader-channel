207bf149a556c89740ab28142029f9bb5b622d15
Drissa Koné <60718751+disco07@users.noreply.github.com>
2022-09-15 15:52:08 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestDownloaderNotError(t *testing.T) {
+func TestDownloader(t *testing.T) {
 	var downloaderTests = []struct {
 		description string
 		url         string
@@ -33,9 +33,10 @@ func TestDownloaderNotError(t *testing.T) {
 			expected:    errors.New("unable to parse variable"),
 		},
 	}
+
 	for _, tt := range downloaderTests {
 		t.Run(tt.description, func(t *testing.T) {
-			err := downloader(tt.url)
+			err := worker(tt.url)
 			if tt.expected == nil && err != nil {
 				t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
 			}
@@ -44,9 +45,11 @@ func TestDownloaderNotError(t *testing.T) {
 				t.Errorf("Unexpected error for input %v: %v (expected %v)", tt.url, err, tt.expected)
 			}
 		})
+	}
+}
 
 func BenchmarkDownloader(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		downloader("https://agritrop.cirad.fr/584726/1/Rapport.pdf")
+		worker("https://agritrop.cirad.fr/584726/1/Rapport.pdf")
 	}
 }
